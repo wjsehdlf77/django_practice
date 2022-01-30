a21b92684ecd853c71d7a0ff7cb66348e9d7340a
doyle <doylehot@gmail.com>
2022-01-31 00:41:01 +0900

    template

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from .models import Members, RendBook
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('방가방가')
+    members_list = Members.objects.order_by('-create_date')
+    context = {
+        'members_list' : members_list
+    }
+
+    return render(request, 'pybo/members_list.html', context)
