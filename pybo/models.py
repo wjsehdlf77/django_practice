5a0c709033320a955bce28cf8aca00e8b7baa357
doyle <doylehot@gmail.com>
2022-01-30 12:21:43 +0900

    models_set_up

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateField()
+
+    def __str__(self):
+        return self
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    def __ste__(self):
+        return self
+
+
+
